441280a50b55768da10685c4675493bf300652ba
AGirlOnSpree <vinijain94@gmail.com>
2022-01-18 11:10:24 +0530

    Combinator Pattern Complete

--- src/main/java/combinatorPattern/Main.java
+++ src/main/java/combinatorPattern/Main.java
@@ -2,15 +2,30 @@ package combinatorPattern;
 
 import java.time.LocalDate;
 
+import static combinatorPattern.CustomerRegistrationValidator.*;
+import static combinatorPattern.CustomerRegistrationValidator.ValidationResult.SUCCESS;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer(
                 "Vinita",
-                "vinijain94@gmail.com",
-                "9021111111",
-                LocalDate.of(1994, 05, 11));
+                "vinijain94gmail.com",
+                "89021111111",
+                LocalDate.of(2019, 05, 11));
 
-        System.out.println(new CustomerValidatorService().isValid(customer));
+//        System.out.println(new CustomerValidatorService().isValid(customer));
 
+        //Using Combinator Pattern
+
+        ValidationResult result = isEmailValid()
+                .and(isAdult())
+                .and(isPhNumValid())
+                .apply(customer);
+
+        System.out.println(result);
+
+        if (result != SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
     }
 }
